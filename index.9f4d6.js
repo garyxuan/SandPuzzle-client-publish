69704a7045aff98a470dce932874e68ace390d1b
wangxiaoxuan <garyxuan@163.com>
2025-10-15 13:13:31 +0800

    bug fix

--- index.9f4d6.js
+++ index.9f4d6.js
@@ -1,4 +1,4 @@
-System.register(["./application.d2c3f.js"], function (_export, _context) {
+System.register(["./application.6c864.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
